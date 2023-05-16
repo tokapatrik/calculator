72a977a587e03d21f6edfd4771062cf901e4e6a8
Patrik <tokapatrik321@gmail.com>
2023-05-16 22:30:55 +0200

    start Html + Css

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="calculator">
+			<div className="output">
+				<div className="previous">asd</div>
+				<div className="current">asd</div>
+			</div>
+			<button className="button-type-1" >AC</button>
+			<button className="button-type-1" >DEL</button>
+			<button className="button-type-1" >%</button>
+			<button className="button-type-3" >/</button>
+			<button className="button-type-2" >1</button>
+			<button className="button-type-2" >2</button>
+			<button className="button-type-2" >3</button>
+			<button className="button-type-3" >X</button>
+			<button className="button-type-2" >4</button>
+			<button className="button-type-2" >5</button>
+			<button className="button-type-2" >6</button>
+			<button className="button-type-3" >+</button>
+			<button className="button-type-2" >7</button>
+			<button className="button-type-2" >8</button>
+			<button className="button-type-2" >9</button>
+			<button className="button-type-3" >-</button>
+			<button className="button-type-2 span-two" >0</button>
+			<button className="button-type-2" >.</button>
+			<button className="button-type-3" >=</button>
+		</div>
+	);
+}
+
+export default App;
